d3ed2bf1cb4978329da26df420ceaba533b9e7a8
NicDelGio <105751936+NicDelGio@users.noreply.github.com>
2022-06-14 20:08:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(path.resolve(__dirname, 'public'))
 );
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
 app.listen(4000, () => {
