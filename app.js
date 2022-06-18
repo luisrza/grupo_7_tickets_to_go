36590764461ff024efa927656ab8123c7df552f8
NicDelGio <nicodelgio04@gmail.com>
2022-06-18 20:38:39 -0300

    hice el contacto.html (revisar)

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const indexPath= path.resolve(__dirname,'./views/index.html');
 const publicPath= path.resolve(__dirname,'./public');
 const indexRegister= path.resolve(__dirname,'./views/register.html');
 const indexLogin= path.resolve(__dirname,'./views/login.html');
+const index404= path.resolve(__dirname,'./views/404.html');
+const indexContacto= path.resolve(__dirname,'./views/contacto.html');
 
 /*npm install express --save <---para bajar express */
 
@@ -31,7 +33,12 @@ app.get('/login', (req,res)=>{
 
 //404
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/404.html'));
+    res.sendFile(path.join(index404));
+}); 
+
+//Contacto //TODO:no esta funcionando, revisar.
+app.get('/contacto', (req, res) => {
+    res.sendFile(path.join(indexContacto));
 }); 
 
 app.listen(port, () => {
