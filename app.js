862d6ae52a2f9458418f4e496872321d07b30917
TapaGonzalez <laulujan@quilmes.com.ar>
2022-06-19 16:20:07 -0300

    nuevos cambios en app

--- app.js
+++ app.js
@@ -10,69 +10,50 @@ const publicPath= path.resolve(__dirname,'./public');
 const registerPath= path.resolve(__dirname,'./views/register.html');
 const contactoPath= path.resolve(__dirname,'./views/contacto.html');
 const loginPath= path.resolve(__dirname,'./views/login.html');
-//const indexEventos= path.resolve(__dirname,'./views/eventos.html');
-//const index404= path.resolve(__dirname,'./views/404.html');
-
-//const indexCarrito= path.resolve(__dirname,'./views/carrito.html');
-//const indexEvento= path.resolve(__dirname,'./views/evento.html');
+const eventosPath= path.resolve(__dirname,'./views/eventos.html');
+const index404Path= path.resolve(__dirname,'./views/404.html');
+const carritoPath= path.resolve(__dirname,'./views/carrito.html');
 
 /*npm install express --save <---para bajar express */
 
 app.use(express.static(publicPath));
-
 //Index
 app.get('/', (req, res) => {
     res.sendFile(indexPath);
 });
-
 //Register
 app.get('/register', (req,res)=>{
     res.sendFile(registerPath);
 });
-
 //Login
 app.get('/login', (req,res)=>{ 
     res.sendFile(loginPath);
 });
-
 //Register
 app.get('/aboutus', (req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/aboutus.html'));
 });
-
 //contacto
 app.get('/contacto', (req, res) => {
     res.sendFile(contactoPath);
 }); 
 
-/*
 //404
 app.get('*', (req, res) => {
-    res.sendFile(index404);
+    res.sendFile(index404Path);
 }); 
 
 //carrito
 app.get('/carrito', (req, res) => {
-    res.sendFile(indexCarrito);
-});
-
-//evento
-app.get('/evento', (req, res) => {
-    res.sendFile(indexEvento);
+    res.sendFile(carritoPath);
 });
 
 //eventos
 app.get('/eventos', (req, res) => {
-    res.sendFile(indexEventos);
+    res.sendFile(eventosPath);
 });
-
-*/
 
 //escuchando
 app.listen(port, () => {
     console.log('listening on http://localhost:'+port);
 });
-
-
-console.log(contactoPath);
-console.log(indexPath);
