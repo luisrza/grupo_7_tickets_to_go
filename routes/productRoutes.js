246aa9323401a12d3bbdf1bf01ebe6b36254e4ed
TapaGonzalez <laulujan@quilmes.com.ar>
2022-07-03 17:14:33 -0300

    cambiando ruteos

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const productController = require('../controllers/productController');
 
-router.get('/', productController.eventos);
+router.get('/eventos', productController.eventos);
 
 module.exports=router;
