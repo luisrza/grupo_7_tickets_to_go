c66da17eb036744253303e7f2f41f0aaefe726ef
TapaGonzalez <laulujan@quilmes.com.ar>
2022-07-04 11:54:59 -0300

    rutas y ejs funcionando

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,15 +2,18 @@ const path = require('path');
 
 const usersController = {
     login: (req, res) => {
-        res.sendFile(path.join(__dirname, '../views/users/login.html'));
+        //res.sendFile(path.join(__dirname, '../views/users/login.html'));
+        res.render('./users/login');
     },
 
     register: (req, res) => {
-        res.sendFile(path.join(__dirname, '../views/users/register.html'));
+        //res.sendFile(path.join(__dirname, '../views/users/register.html'));
+        res.render('./users/register');
     },
 
     carrito: (req, res) => {
-        res.sendFile(path.join(__dirname, '../views/users/carrito.html'));
+        //res.sendFile(path.join(__dirname, '../views/users/carrito.html'));
+        res.render('./users/carrito');
     },
     
 
