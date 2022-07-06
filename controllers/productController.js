4d8c9d7db96c6c16104fb5e6378a1d92a4ca8193
diegofranc <diegofranco2001@gmail.com>
2022-07-05 23:36:13 -0300

    dinamize pag eventos

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const eventos = require ('./eventos');
 
 const productController = {
     eventos: (req, res) => {
         //res.sendFile(path.join(__dirname, '../views/products/eventos.html'));
-        res.render('./products/eventos')
+        res.render('./products/eventos',{eventos})
     },
 
     dinamicas: (req, res) => {
