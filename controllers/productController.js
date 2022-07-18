e23d49041b9de1d0208eda69bc6e4068e921423f
TapaGonzalez <laulujan@quilmes.com.ar>
2022-07-18 20:58:11 -0300

    actualizando el crud

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,25 +1,97 @@
+
+const fs = require('fs');
 const path = require('path');
-const eventos = require ('./eventos');
+
+const productsFilePath = path.join(__dirname, '../data/eventos.json');
+var eventos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const productController = {
     eventos: (req, res) => {
-        //res.sendFile(path.join(__dirname, '../views/products/eventos.html'));
+        
         res.render('./products/eventos',{eventos})
     },
-    dinamicas: (req, res) => {
-        let idProductos= req.params.idProductos;
-        res.send("ALOHA"+idProductos);
+    detalleEventos: (req, res) => {
+        let place = req.params.id;
+        for (i=0;i<eventos.length;i++){
+            if(eventos[i].place == place)
+            {evento=eventos[i]}
+        }
+        
+        res.render('./products/detallesEvento',{evento})
     },
+   
     crearEvento: (req, res) => {
         res.render('./products/crearEvento')
     },
-    editarEvento: (req, res) => {
-        let idEvento= req.params.idEvento
-        //res.send(idEvento);
+    creandoEvento: (req, res) => {
+        let newEvento = req.body;
+        
 
-        let eventoToEdit= eventos [idEvento];
-        //res.send(EventoToEdit);
-        res.render('./products/editarEvento', {eventoToEdit:eventoToEdit});
+        newEvento.id = Date.now();
+        newEvento.agotado = "No"
+
+        let imagen = req.file;
+		newEvento.image = imagen.filename;
+		
+   
+        eventos.push(newEvento);
+
+        fs.writeFileSync(productsFilePath, JSON.stringify(eventos, null, 2));
+
+        res.redirect('eventos');
+
+
+      
+    },
+    editarEvento: (req, res) => {
+        let id= req.params.id
+        
+        for(i=0;i<eventos.length;i++){
+            if(eventos[i].id==id){
+                eventoToEdit=eventos[i];
+            }
+        }
+        
+        res.render('./products/editarEvento', {eventoToEdit});
+           
+    },
+    editandoEvento: (req, res) => {
+        let eventoEditado = req.body;
+        
+        let imagen = req.file;
+        eventoEditado.image = imagen.filename
+		
+		let eventosNew = [];
+        for (i=0;i<eventos.length;i++){
+            if (eventoEditado.id != eventos[i].id){
+               eventosNew.push(eventos[i])  
+             };
+         }
+           
+         eventosNew.push(eventoEditado);
+
+         eventos = eventosNew;
+
+         fs.writeFileSync(productsFilePath, JSON.stringify(eventos, null, 2));
+        
+         res.redirect('/products/eventos');
+           
+    },
+    eliminarEvento: (req, res) => {
+        
+        id = req.params.id
+        newEventos = [];
+        for (i=0;i<eventos.length;i++) {
+            if (eventos[i].id!=id){
+                newEventos.push(eventos[i])
+            }
+        }
+        eventos = newEventos;
+
+        fs.writeFileSync(productsFilePath, JSON.stringify(eventos, null, 2));
+
+        res.redirect('/products/eventos');
+           
     }
 
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,15 +1,33 @@
 const express = require ('express');
 const router = express.Router();
 
+const multer = require('multer');
+const path = require('path');
 const productController = require('../controllers/productController');
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../public/img'))
+    },
+    filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+    }
+    })
+var upload = multer({ storage: storage })
+
+// rutas para eventos y detalle de eventos
 router.get('/eventos', productController.eventos);
+router.get('/eventos/detalles/:id', productController.detalleEventos);
 
+//rutas para crear eventos 
 router.get('/crearEvento', productController.crearEvento);
+router.post('/creandoEvento', upload.single('image'), productController.creandoEvento);
 
-router.get('/:idProductos', productController.dinamicas);
+//rutas para editar eventos
+router.get('/editarEvento/:id', productController.editarEvento);
+router.put('/editandoEvento', upload.single('image'), productController.editandoEvento);
 
-router.get('/editarEvento/:idEvento', productController.editarEvento);
-router.put('/editarEvento/', productController.editarEvento);
+//ruta para eliminar evento
+router.delete('/eliminarEvento/:id', productController.eliminarEvento);
 
 module.exports=router;
