4b348332cc5a36e14e00c7dfff8c0ba6e204dada
diegofranc <diegofranco2001@gmail.com>
2022-07-29 15:46:12 -0300

    crud para users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,99 @@
 const path = require('path');
+const fs = require('fs');
+const usersFilePath = path.join(__dirname, '../data/users.json');
+var users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const usersController = {
     login: (req, res) => {
-        //res.sendFile(path.join(__dirname, '../views/users/login.html'));
         res.render('./users/login');
     },
+    logueando: (req, res) => {
+        
+     email = req.body.email
+     noExiste = true
+    
+     for (i=0;i<users.length;i++){
+        if (users[i].email==email){
+         userLogin = users[i]
+        noExiste=false
+         res.render('users/detalleUsuario',{userLogin})
+     } 
+    } 
+     if (noExiste == true){res.redirect('usuarioNoExiste')}
+    },
+    detalleUsuario: (req,res) => {
+        res.render('users/detalleUsuario')
 
+    },
+    editarUsuario: (req,res) => {
+
+        id = req.params.id
+        
+        for (i=0;i<users.length;i++){
+            if (users[i].id==id){
+                userToEdit = users[i]
+            }
+        }
+
+    res.render('users/editarUsuario',{userToEdit})
+
+        
+    },    
+    editandoUsuario: (req,res) => {
+
+        id = req.params.id
+        console.log(id)
+        for (i=0;i<users.length;i++){
+            if (users[i].id==id){
+                userToEdit = users[i]
+            }
+        }
+        userEdited = {
+            id: userToEdit.id,
+            nombre: req.body.nombre,
+            user: req.body.user,
+            nacimiento: req.body.nacimiento,
+            pass: req.body.pass,
+            avatar: userToEdit.avatar
+
+        }
+        if (req.file) {userEdited.avatar = avatar.filename}
+       newUsers = [];
+       newUsers.push(userEdited)
+        for (i=0;i<users.length;i++){
+            if (users[i].id!=id){
+                newUsers.push(users[i]) 
+            }
+        }
+        users= newUsers
+        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+        userLogin = userEdited
+
+        
+       res.render('users/detalleUsuario',{userLogin})
+
+        
+    },  
+    eliminarUsuario: (req,res) => {
+
+        id = req.params.id
+        newUsers=[]
+        for (i=0;i<users.length;i++){
+            if (users[i].id!=id){
+                newUsers.push(users[i])
+            }
+        }
+        users = newUsers
+        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+        res.render('users/eliminarUsuario')
+
+    },
+    usuarioNoExiste: (req,res) => {
+        res.render('users/usuarioNoExiste')
+
+    },
     register: (req, res) => {
-        //res.sendFile(path.join(__dirname, '../views/users/register.html'));
+        
         res.render('./users/register');
     },
 
@@ -17,17 +103,28 @@ const usersController = {
     },
     
     create: (req, res) => {
-        //res.sendFile(path.join(__dirname, '../views/users/carrito.html'));
-        //res.render('./users/carrito');
-        //res.send(req.body);
-        let usuario = {
+       
+        let newUsuario = {
+            id: Date.now(),
             nombre: req.body.nombre,
-            user:  req.body.user,  
+            user: req.body.user,  
+            nacimiento: req.body.nacimiento,
+            email: req.body.email,
+            password: req.body.pass
         }
-        // Hay que guardarla en algun lado.
-        res.send(usuario);
-        // res.redirect('/users/list'); aca vamos a un listado de usuario dnd guardariamos todo.
+        let avatar = req.file;
+		newUsuario.avatar = avatar.filename;
+        
+        users.push(newUsuario)
+        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+       
+        res.redirect('./registrado'); 
 
+    },
+
+    registrado:  (req, res) => {
+       
+        res.render('./users/registrado')
     },
 
     editar: (req, res) => {
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,15 +1,33 @@
 const express = require ('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
 const usersController = require('../controllers/usersController');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../public/img'))
+    },
+    filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+    }
+    })
+var upload = multer({ storage: storage })
 
 router.get('/carrito', usersController.carrito);
 
 router.get('/login', usersController.login);
+router.post('/logueando', usersController.logueando);
+router.get('/usuarioNoExiste', usersController.usuarioNoExiste);
+router.get('/detalleUsuario/:id', usersController.detalleUsuario);
+router.get('/eliminarUsuario/:id', usersController.eliminarUsuario);
+router.get('/editarUsuario/:id', usersController.editarUsuario);
+router.post('/editandoUsuario/:id',upload.single('avatar'), usersController.editandoUsuario);
 
 router.get('/register', usersController.register);
-router.post('/register', usersController.create);
-
+router.post('/register',  upload.single('avatar'), usersController.create);
+router.get('/registrado', usersController.registrado);
 
 // parametros para el PUT
 //router.get('/:id/editar', usersController.editar);
