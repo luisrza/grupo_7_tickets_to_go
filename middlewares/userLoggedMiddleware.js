542e8105bfce721f76c7f739e90afd4ba6ea6b9b
diegofranc <diegofranco2001@gmail.com>
2022-08-06 21:34:08 -0300

    corrigiendo detalles cookies

--- middlewares/userLoggedMiddleware.js
+++ middlewares/userLoggedMiddleware.js
@@ -5,18 +5,18 @@ var users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 function userLoggedMiddleware (req, res, next){
     res.locals.isLogged = false;
-    //console.log(req.cookies.emailCookie)
+    
     let emailInCookie = req.cookies.emailCookie
     let userFromCookie=undefined
     for (i=0;i<users.length;i++){
         if (users[i].email==emailInCookie){
             userFromCookie = users[i]
-            console.log(userFromCookie)
+            
         }
     }
     if (userFromCookie!=undefined){
         req.session.userLogged = userFromCookie
-        console.log(userFromCookie)
+        
     }
 
     if (req.session.userLogged){
