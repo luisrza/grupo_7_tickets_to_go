77d8a82976cce6d0f85e6d2f56fddb436f7bb4a0
NicDelGio <nicodelgio04@gmail.com>
2022-10-08 11:59:14 -0300

    carlos saul

---
+++ public/js/burger.js
@@ -0,0 +1,18 @@
+//window.addEventListener("load", function(){
+
+    // console.log("cfgvhbjnk");
+
+    // const burger = document.querySelector(".burger");
+    // const navbar = document.querySelector(".nav-bar");
+
+    // burger.addEventListener("click", function(e){
+
+    //     const punto = e.target;
+    //     console.log(punto);
+    //     alert("click");
+    //     burger.style.display = "none";
+    //     navbar.style.display = "flex";
+    // })
+
+//})
+
