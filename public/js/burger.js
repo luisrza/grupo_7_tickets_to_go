416e9a209535c9dfa1a92dbfa5cdca346862e264
NicDelGio <nicodelgio04@gmail.com>
2022-10-11 19:21:55 -0300

    burgir

--- public/js/burger.js
+++ public/js/burger.js
@@ -1,18 +1,24 @@
 window.addEventListener("load", function(){
 
-    console.log("cfgvhbjnk");
-
     const burger = document.querySelector(".burger");
     const navbar = document.querySelector(".nav-bar");
+    const arrowOut = document.querySelector(".arrowOut");
 
     burger.addEventListener("click", function(e){
 
         const punto = e.target;
-        console.log(punto);
-        alert("click");
         burger.style.display = "none";
         navbar.style.display = "flex";
+        arrowOut.style.display = "flex";
     })
 
+    arrowOut.addEventListener("click", function(e){
+
+        const punto = e.target;
+        burger.style.display = "block";
+        navbar.style.display = "none";
+        arrowOut.style.display = "none";
+    })
 })
+
 
