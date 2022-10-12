39dc6d08d52dd6e2a032a660febdab6ba880043f
luisrza <werox51@gmail.com>
2022-10-11 19:49:45 -0500

    admin hide

--- public/js/validacion.js
+++ public/js/validacion.js
@@ -8,15 +8,32 @@ window.addEventListener("load", function(){
     const password = document.querySelector('#password');
     const terms = document.querySelector('#terms');
     const pass_min = 6;
+    const admin = document.querySelector('.admin');
+    const ticket ="@ticketstogo.com";
+
+    form.addEventListener('click', function(){
+        let correo = email.value;
+        if (correo === "@ticketstogo.com"){
+            admin.style.display = 'block';
+        }
+    })
 
     form.addEventListener('submit', function(evento){
         evento.preventDefault();
         if (nombre.value == ""){
             console.log('El nombre no puede estar vacio');
+            const name = document.querySelector('.nombre-hidden');
+            name.style.display = 'block';
+        }
+        if (user.value == ""){
+            console.log('El usuario no puede estar vacio');
+            const usuario = document.querySelector('.usuario-hidden');
+            usuario.style.display = 'block';
         }
         if (password.value.length < pass_min){
-            console.log('Hubo un error en el password')
-            
+            console.log('Hubo un error en el password');
+            const hidden = document.querySelector('.pass-hidden');
+            hidden.style.display = 'block';
         }
         nombre.addEventListener('focus', function(){
             console.log('hola');
