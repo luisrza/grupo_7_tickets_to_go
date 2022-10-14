229713b0a41101a2e16fe96501b3394de9493ab6
luisrza <werox51@gmail.com>
2022-10-13 20:37:44 -0500

    prevent default missing

--- public/js/validacion.js
+++ public/js/validacion.js
@@ -3,23 +3,26 @@ window.addEventListener("load", function(){
     const form = document.querySelector('#form_reg');
     const nombre = document.querySelector("#nombre");
     const user = document.querySelector('#user')
-    const dob = document.querySelector('#dob');
+    const dob = document.querySelector('#fechaNacimiento');
     const email = document.querySelector('#email');
     const password = document.querySelector('#password');
     const terms = document.querySelector('#terms');
     const pass_min = 6;
     const admin = document.querySelector('.admin');
     const ticket ="@ticketstogo.com";
+    const dob_string = dob.value.split("/");
+    const d = new Date();
+    const y = d.getFullYear();
 
-    form.addEventListener('click', function(){
+    form.addEventListener('keyup', function(){
         let correo = email.value;
-        if (correo === "@ticketstogo.com"){
+        if (correo === ticket){
             admin.style.display = 'block';
         }
     })
 
     form.addEventListener('submit', function(evento){
-        evento.preventDefault();
+        preventDefault()
         if (nombre.value == ""){
             console.log('El nombre no puede estar vacio');
             const name = document.querySelector('.nombre-hidden');
@@ -34,10 +37,11 @@ window.addEventListener("load", function(){
             console.log('Hubo un error en el password');
             const hidden = document.querySelector('.pass-hidden');
             hidden.style.display = 'block';
+        } 
+        if (parseInt(dob_string[0]) > y -18) 
+        {
+            const hidden = document.querySelector('.dob-hidden');
+            hidden.style.display = 'block';
         }
-        nombre.addEventListener('focus', function(){
-            console.log('hola');
-            nombre.style.backgroundColor = 'pearl';
-        })
     })
 })
