64a103b26b2ab2ae9f9281363aa20807050029d4
luisrza <werox51@gmail.com>
2022-10-14 00:27:32 -0500

    validaciones dob

--- public/js/validacion.js
+++ public/js/validacion.js
@@ -6,24 +6,62 @@ window.addEventListener("load", function(){
     const dob = document.querySelector('#fechaNacimiento');
     const email = document.querySelector('#email');
     const password = document.querySelector('#password');
+    const confirmacion = document.querySelector('#confirmacion');
     const terms = document.querySelector('#terms');
     const pass_min = 6;
     const admin = document.querySelector('.admin');
     const ticket ="@ticketstogo.com";
-    const dob_string = dob.value.split("/");
-    const d = new Date();
-    const y = d.getFullYear();
 
-    form.addEventListener('keyup', function(){
+    email.addEventListener('keyup', function(){
         let correo = email.value;
         if (correo === ticket){
             admin.style.display = 'block';
         }
     })
 
+    confirmacion.addEventListener('keyup', function(){
+        if (password.value.length < pass_min){
+            console.log('Hubo un error en el password');
+            const hidden = document.querySelector('.pass-hidden');
+            hidden.style.display = 'block';
+        }
+        if (password.value.length === confirmacion.value.length){
+            const hidden = document.querySelector('.pass2-hidden');
+            hidden.style.display = 'none';
+        }
+        if (password.value.length !== confirmacion.value.length){
+            console.log('Hubo un error en el password');
+            const hidden = document.querySelector('.pass2-hidden');
+            hidden.style.display = 'block';
+        }
+    })
+
+    form.addEventListener('input', function(){
+        const dob_string = dob.value.split("/");
+        const d = new Date();
+        const y = d.getFullYear(); 
+        if (parseInt(dob_string[0]) <= y -18) {
+            const hidden = document.querySelector('.dob-hidden');
+            hidden.style.display = 'none';
+        }
+        if (parseInt(dob_string[0]) > y -18) {  
+            const hidden = document.querySelector('.dob-hidden');
+            hidden.style.display = 'block';
+        }
+        if (nombre.value.length > 1){
+            console.log('El nombre no puede estar vacio');
+            const name = document.querySelector('.nombre-hidden');
+            name.style.display = 'none';
+        }
+        if (password.value.length > pass_min){
+            console.log('Hubo un error en el password');
+            const hidden = document.querySelector('.pass-hidden');
+            hidden.style.display = 'none';
+        } 
+    })
+
     form.addEventListener('submit', function(evento){
-        preventDefault()
-        if (nombre.value == ""){
+        if (nombre.value === ""){
             console.log('El nombre no puede estar vacio');
             const name = document.querySelector('.nombre-hidden');
             name.style.display = 'block';
@@ -33,15 +71,11 @@ window.addEventListener("load", function(){
             const usuario = document.querySelector('.usuario-hidden');
             usuario.style.display = 'block';
         }
-        if (password.value.length < pass_min){
-            console.log('Hubo un error en el password');
-            const hidden = document.querySelector('.pass-hidden');
-            hidden.style.display = 'block';
-        } 
-        if (parseInt(dob_string[0]) > y -18) 
-        {
-            const hidden = document.querySelector('.dob-hidden');
-            hidden.style.display = 'block';
+        if (email.value === ""){
+            console.log('El nombre no puede estar vacio');
+            const email = document.querySelector('.email-hidden');
+            email.style.display = 'block';
         }
+        evento.preventDefault();
     })
 })
